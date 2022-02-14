078a77820cf30312ca571864f86115bcca239c1b
Rogozhinskii <rogozhinskii96@mail.ru>
2022-02-14 17:16:39 +0500

    Добавьте файлы проекта.

---
+++ Converter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
